95ec83b03095a27c0019f75e5d761cfc9c46c841
precious hope <145859632+precioushopey@users.noreply.github.com>
2023-09-27 00:14:48 +0800

    feat: initial homepage (#6)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,19 @@ import logo from '../assets/images/logo.png'
 
 function Navbar() {
   return (
-    <div className='bg-[#FEC51C] w-full h-24 rounded-b-[48px] px-24'>
-        <div className='flex flex-row h-full justify-between items-center'>
+    <div className='bg-[#FEC51C] w-full rounded-b-[30px] h-16 px-16 fixed top-0'>
+        <div className='flex h-full justify-between items-center'>
             <img src={logo} className='h-[45px] w-[145px] cursor-pointer'/>
-            <div className='flex flex-row items-center text-[#1F2F3D] font-arimo font-bold cursor-pointer gap-12'>
+            <div className='flex items-center text-[#1F2F3D] font-arimo font-bold cursor-pointer gap-12'>
                 <p>ABOUT US</p>
                 <p>CITIZEN'S CHARTER</p>
                 <p>SERVICES</p>
                 <p>CONTACT US</p>
                 <p className='text-[#1B75BC]'>LOGIN</p>
-                <div className='bg-[#1F2F3D] text-white rounded-[10px] py-2 px-5'>
-                    SIGN UP
-                </div>
+                <p className='bg-[#1F2F3D] text-white rounded-[10px] py-2 px-5'>SIGN UP</p>
             </div>
         </div>
     </div>
-
   )
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import logo from '../assets/images/logo.png'
+import brand from '../assets/images/branding.png'
+import backg from '../assets/images/background.svg'
 
 function Homepage() {
   return (
     <div>
-        <Navbar />
+      <Navbar />
+      <div className='w-full h-screen flex flex-col justify-between'>
+        <div className='flex flex-row max-w-[1248px] m-auto'>
+          <div>
+            <img src={brand}/>
+          </div>
+          <div className='text-[#1F2F3D] font-arimo flex flex-col justify-center md:items-start w-full'>
+            <h1 className='text-4xl font-bold ml-12'>WELCOME TO</h1>
+            <img src={logo} class='scale-75 -ml-10 -mt-7'/>
+            <p class='italic text-xl ml-12 -mt-8'>True service, tailored to your needs!</p>
+            <div className='flex flex-row items-center gap-1 mt-8'>
+              <button className='bg-[#1F2F3D] text-white rounded-[50px] ml-12 py-2 px-20 shadow-2xl'>
+                    SIGN UP
+              </button>
+              <button className='bg-[#FFFFFF] text-[#1F2F3D] border-2 border-[#1F2F3D] rounded-[50px] ml-12 py-2 px-20 shadow-2xl'>
+                    LOG IN
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img src={backg} className='fixed bottom-0'/>
     </div>
   )
 }
